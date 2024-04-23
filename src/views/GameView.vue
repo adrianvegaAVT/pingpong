503bed7cc479c043548507f163605b6d5416c7c1
Adrian Vega <adrianvegat@gmail.com>
2024-04-22 20:33:36 -0600

    inicio de estilos en gameview

--- src/views/GameView.vue
+++ src/views/GameView.vue
@@ -1,31 +1,56 @@
 <template>
-    <div>
-        <h1>{{equipo1}} vs {{equipo2}}</h1>
-        <h2>{{puntosEquipo1}} - {{puntosEquipo2}}</h2>
-        <button @click="puntoEquipo1">Punto {{equipo1}}</button>
-        <button @click="puntoEquipo2">Punto {{equipo2}}</button>
-    </div>
+	<div class="col-sm-12 col-xl-3 col-lg-3 col-md-6 m-4">
+		<div class="d-flex justify-content-between align-items-center">
+			<div>
+				<a class="btn btn-secondary" href="/">Regresar</a>
+			</div>
+			<img class="img-log" src="img/local/uped-logo.png" alt="" />
+		</div>
+
+        <div class="div-set-num">
+            <p class="h1 p-4 text-center">Set #</p>
+        </div>
+
+        <div class="div-puntaje rounded p-3" >
+            <p class="h4 text-center bold">Puntos</p>
+            <div class="d-flex justify-content-around">
+                <div>
+                    <p class="points text-center">{{ puntosEquipo1 }}</p>
+                    <button class="mx-auto">+</button>
+                </div>
+                <div>
+                    <p class="points text-center">{{ puntosEquipo2 }}</p>
+                    <button>+</button>
+                </div>
+            </div>
+        </div>
+
+		<h1>{{ equipo1 }} vs {{ equipo2 }}</h1>
+		<h2>{{ puntosEquipo1 }} - {{ puntosEquipo2 }}</h2>
+		<button @click="puntoEquipo1">Punto {{ equipo1 }}</button>
+		<button @click="puntoEquipo2">Punto {{ equipo2 }}</button>
+	</div>
 </template>
 
 <script>
 export default {
-  data() {
-    return {
-      puntosEquipo1: localStorage.getItem('puntosEquipo1') || 0,
-      puntosEquipo2: localStorage.getItem('puntosEquipo2') || 0,
-      equipo1: this.$route.query.equipo1,
-      equipo2: this.$route.query.equipo2
-    }
-  },
-  methods: {
-    puntoEquipo1() {
-      this.puntosEquipo1++
-      localStorage.setItem('puntosEquipo1', this.puntosEquipo1)
-    },
-    puntoEquipo2() {
-      this.puntosEquipo2++
-      localStorage.setItem('puntosEquipo2', this.puntosEquipo2)
-    }
-  }
-}
+	data() {
+		return {
+			puntosEquipo1: localStorage.getItem("puntosEquipo1") || 0,
+			puntosEquipo2: localStorage.getItem("puntosEquipo2") || 0,
+			equipo1: this.$route.query.equipo1,
+			equipo2: this.$route.query.equipo2,
+		};
+	},
+	methods: {
+		puntoEquipo1() {
+			this.puntosEquipo1++;
+			localStorage.setItem("puntosEquipo1", this.puntosEquipo1);
+		},
+		puntoEquipo2() {
+			this.puntosEquipo2++;
+			localStorage.setItem("puntosEquipo2", this.puntosEquipo2);
+		},
+	},
+};
 </script>
--- src/views/SetupView.vue
+++ src/views/SetupView.vue
@@ -1,43 +1,66 @@
 <template>
-    <div>
-        <label for="">Equipo / Jugador 1</label>
-        <input type="text" id="equipo1" v-model="equipo1" />
-    </div>
-    <div>
-        <label for="">Equipo / Jugador 2</label>
-        <input type="text" id="equipo2" v-model="equipo2"/>
-    </div>
-    <button type="button" @click="iniciarJuego">Iniciar Juego</button>
-    <p>{{error}}</p>
+	<div class="col-sm-12 col-xl-3 col-lg-3 col-md-6 m-4">
+		<div class="d-flex justify-content-end">
+			<img class="img-log" src="img/local/uped-logo.png" alt="" />
+		</div>
+		<div class="mt-4">
+			<label for="" class="fs-2 fw-bold">Equipo / Jugador 1</label>
+			<input
+				type="text"
+				id="equipo1"
+				v-model="equipo1"
+				class="form-control mt-3"
+			/>
+		</div>
+		<div class="mt-4 mb-4">
+			<label for="" class="fs-2 fw-bold">Equipo / Jugador 2</label>
+			<input
+				type="text"
+				id="equipo2"
+				v-model="equipo2"
+				class="form-control mt-3"
+			/>
+		</div>
+		<div class="mt-4 d-flex justify-content-center">
+			<button
+				type="button"
+				@click="iniciarJuego"
+				class="btn btn-primary mt-4 btn-iniciar-juego"
+			>
+				Iniciar Juego
+			</button>
+		</div>
+		<p>{{ error }}</p>
+	</div>
 </template>
 
 <script>
 export default {
-  data() {
-    return {
-      equipo1: '',
-      equipo2: '',
-      error: ''
-    }
-  },
-  methods: {
-    iniciarJuego() {
-      if (!this.equipo1 || !this.equipo2) {
-        this.error = 'Por favor, introduce los nombres de ambos equipos';
-        return;
-      }
+	data() {
+		return {
+			equipo1: "",
+			equipo2: "",
+			error: "",
+		};
+	},
+	methods: {
+		iniciarJuego() {
+			if (!this.equipo1 || !this.equipo2) {
+				this.error = "Por favor, introduce los nombres de ambos equipos";
+				return;
+			}
 
-      localStorage.setItem('puntosEquipo1', 0);
-      localStorage.setItem('puntosEquipo2', 0);
-      this.$router.push({
-        name: 'game',
-        query: {
-          equipo1: this.equipo1,
-          equipo2: this.equipo2
-        }
-      })
-    }
-  }
-}
+			localStorage.setItem("puntosEquipo1", 0);
+			localStorage.setItem("puntosEquipo2", 0);
+			this.$router.push({
+				name: "game",
+				query: {
+					equipo1: this.equipo1,
+					equipo2: this.equipo2,
+				},
+			});
+		},
+	},
+};
 //modificacion de prueba
 </script>
